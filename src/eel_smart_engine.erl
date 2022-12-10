f4f55506b10e4be530335e023c98c1ccbbf8e91d
williamthome <willegp88@gmail.com>
2022-12-10 01:03:47 -0300

    feat: reduce complexity

--- src/eel_smart_engine.erl
+++ src/eel_smart_engine.erl
@@ -16,35 +16,9 @@
 -endif.
 
 %% Defines
--define(vars(Bin), begin
-    {ok, Tokens, _} = erl_scan:string(erlang:binary_to_list(Bin)),
-    MaybeAcc =
-        fun(Var, VAcc) ->
-            case lists:member(Var, VAcc) of
-                true -> VAcc;
-                false -> [Var | VAcc]
-            end
-        end,
-    {_, Vars} =
-        lists:foldl(
-            fun
-                ({var, _, Var} = Token, {[{':=', _} | _] = TAcc, VAcc}) ->
-                    {[Token | TAcc], MaybeAcc(Var, VAcc)};
-                ({var, _, _} = Token, {[{'=>', _} | _] = TAcc, VAcc}) ->
-                    {[Token | TAcc], VAcc};
-                ({var, _, Var} = Token, {TAcc, VAcc}) ->
-                    {[Token | TAcc], MaybeAcc(Var, VAcc)};
-                (Token, {TAcc, VAcc}) ->
-                    {[Token | TAcc], VAcc}
-            end,
-            {[], []},
-            Tokens
-        ),
-    Vars
-end).
 -define(token(Name), fun
-    ({Ln, Col}, {_, Bin}) -> {{Ln, Col}, {Name, Bin}, ?vars(Bin)};
-    ({Ln, Col}, Bin) -> {{Ln, Col}, {Name, Bin}, []}
+    ({_, Bin}) -> {Name, Bin};
+    (Bin) -> {Name, Bin}
 end).
 -define(text, ?token(text)).
 -define(expr, ?token(expr)).
@@ -65,25 +39,25 @@ end).
 init([]) ->
     {ok, #state{}}.
 
-handle_expr({{_Ln, _Col}, {<<"=">>, <<".">>}, {_ExpOut, <<>>}}, State) ->
+handle_expr({_Pos, {<<"=">>, <<".">>}, {_ExpOut, <<>>}}, State) ->
     {ok, State};
-handle_expr({Pos, {<<"=">>, <<".">>}, Expr}, State) ->
-    {ok, push(?expr(Pos, Expr), State)};
-handle_expr({Pos, {<<"=">>, <<"">>}, Expr}, State) ->
-    {ok, push(?start(Pos, Expr), State)};
-handle_expr({Pos, {<<"">>, <<"">>}, Expr}, State) ->
-    {ok, push(?mid(Pos, Expr), State)};
-handle_expr({Pos, {<<"">>, <<".">>}, Expr}, State) ->
-    {ok, push(?'end'(Pos, Expr), State)};
-handle_expr({Pos, {<<":">>, <<".">>}, Expr}, State) ->
-    {ok, push(?echo(Pos, Expr), State)};
-handle_expr({{_Ln, _Col}, {<<"%">>, <<>>}, {_ExpOut, _ExpIn}}, State) ->
+handle_expr({_Pos, {<<"=">>, <<".">>}, Expr}, State) ->
+    {ok, push(?expr(Expr), State)};
+handle_expr({_Pos, {<<"=">>, <<>>}, Expr}, State) ->
+    {ok, push(?start(Expr), State)};
+handle_expr({_Pos, {<<>>, <<>>}, Expr}, State) ->
+    {ok, push(?mid(Expr), State)};
+handle_expr({_Pos, {<<>>, <<".">>}, Expr}, State) ->
+    {ok, push(?'end'(Expr), State)};
+handle_expr({_Pos, {<<":">>, <<".">>}, Expr}, State) ->
+    {ok, push(?echo(Expr), State)};
+handle_expr({_Pos, {<<"%">>, <<>>}, _Expr}, State) ->
     {ok, State};
 handle_expr(Token, State) ->
     {error, {unknown_marker, {Token, State}}}.
 
-handle_text({Pos, Text}, State) ->
-    {ok, push(?text(Pos, Text), State)}.
+handle_text({_Pos, Text}, State) ->
+    {ok, push(?text(Text), State)}.
 
 handle_body(#state{tokens = Tokens}) ->
     {ok, lists:reverse(Tokens)}.
@@ -94,6 +68,33 @@ handle_body(#state{tokens = Tokens}) ->
 
 push(Token, #state{tokens = Tokens}) ->
     #state{tokens = [Token | Tokens]}.
+
+% TODO: Retrieve funs vars
+% retrieve_vars(Bin) ->
+%     {ok, Tokens, _} = erl_scan:string(erlang:binary_to_list(Bin)),
+%     MaybeAcc =
+%         fun(Var, VAcc) ->
+%             case lists:member(Var, VAcc) of
+%                 true -> VAcc;
+%                 false -> [Var | VAcc]
+%             end
+%         end,
+%     {_, Vars} =
+%         lists:foldl(
+%             fun
+%                 ({var, _, Var} = Token, {[{':=', _} | _] = TAcc, VAcc}) ->
+%                     {[Token | TAcc], MaybeAcc(Var, VAcc)};
+%                 ({var, _, _} = Token, {[{'=>', _} | _] = TAcc, VAcc}) ->
+%                     {[Token | TAcc], VAcc};
+%                 ({var, _, Var} = Token, {TAcc, VAcc}) ->
+%                     {[Token | TAcc], MaybeAcc(Var, VAcc)};
+%                 (Token, {TAcc, VAcc}) ->
+%                     {[Token | TAcc], VAcc}
+%             end,
+%             {[], []},
+%             Tokens
+%         ),
+%     Vars.
 
 %%%=============================================================================
 %%% Tests
@@ -113,35 +114,35 @@ handle_expr_test() ->
         {
             "Should return expr token",
             ?assertEqual(
-                {ok, #state{tokens = [{{1, 1}, {expr, <<"Foo">>}, ['Foo']}]}},
+                {ok, #state{tokens = [{expr, <<"Foo">>}]}},
                 handle_expr({{1, 1}, {<<"=">>, <<".">>}, {<<"<%= Foo .%>">>, <<"Foo">>}}, #state{})
             )
         },
         {
             "Should return start_expr token",
             ?assertEqual(
-                {ok, #state{tokens = [{{1, 1}, {start_expr, <<"Foo">>}, ['Foo']}]}},
-                handle_expr({{1, 1}, {<<"=">>, <<"">>}, {<<"<%= Foo %>">>, <<"Foo">>}}, #state{})
+                {ok, #state{tokens = [{start_expr, <<"Foo">>}]}},
+                handle_expr({{1, 1}, {<<"=">>, <<>>}, {<<"<%= Foo %>">>, <<"Foo">>}}, #state{})
             )
         },
         {
             "Should return mid_expr token",
             ?assertEqual(
-                {ok, #state{tokens = [{{1, 1}, {mid_expr, <<"Foo">>}, ['Foo']}]}},
-                handle_expr({{1, 1}, {<<"">>, <<"">>}, {<<"<% Foo %>">>, <<"Foo">>}}, #state{})
+                {ok, #state{tokens = [{mid_expr, <<"Foo">>}]}},
+                handle_expr({{1, 1}, {<<>>, <<>>}, {<<"<% Foo %>">>, <<"Foo">>}}, #state{})
             )
         },
         {
             "Should return end_expr token",
             ?assertEqual(
-                {ok, #state{tokens = [{{1, 1}, {end_expr, <<"Foo">>}, ['Foo']}]}},
-                handle_expr({{1, 1}, {<<"">>, <<".">>}, {<<"<% Foo .%>">>, <<"Foo">>}}, #state{})
+                {ok, #state{tokens = [{end_expr, <<"Foo">>}]}},
+                handle_expr({{1, 1}, {<<>>, <<".">>}, {<<"<% Foo .%>">>, <<"Foo">>}}, #state{})
             )
         },
         {
             "Should return echo token",
             ?assertEqual(
-                {ok, #state{tokens = [{{1, 1}, {echo, <<"io:format(Foo)">>}, ['Foo']}]}},
+                {ok, #state{tokens = [{echo, <<"io:format(Foo)">>}]}},
                 handle_expr({{1, 1}, {<<":">>, <<".">>}, {<<"<%: io:format(Foo) .%>">>, <<"io:format(Foo)">>}}, #state{})
             )
         },
@@ -166,7 +167,7 @@ handle_text_test() ->
         {
             "Should return text token",
             ?assertEqual(
-                {ok, #state{tokens = [{{1, 1}, {text, <<"Foo">>}, []}]}},
+                {ok, #state{tokens = [{text, <<"Foo">>}]}},
                 handle_text({{1, 1}, <<"Foo">>}, #state{})
             )
         }
@@ -196,29 +197,29 @@ handle_body_test() ->
             "</p>"
         "<% end .%>"
     >>,
-    Expected =  [
-        {{1,1},{text,<<"<h1>Title</h1>">>},[]},
-        {{1,15},{start_expr,<<"case 1 of">>},[]},
-        {{1,31},{mid_expr,<<"2 ->">>},[]},
-        {{1,41},{text,<<"<p>Foo</p>">>},[]},
-        {{1,51},{mid_expr,<<"; Bar ->">>},['Bar']},
-        {{1,65},{echo,<<"io:format(\"Print but not render me!~n\")">>},[]},
-        {{1,112},{text,<<"<p>">>},[]},
-        {{1,115},{start_expr,<<"case hello =:= world of">>},[]},
-        {{1,145},{mid_expr,<<"true ->">>},[]},
-        {{1,158},{expr,<<"hello">>},[]},
-        {{1,171},{mid_expr,<<"; false ->">>},[]},
-        {{1,187},{text,<<"<p>">>},[]},
-        {{1,190},{start_expr,<<"case car =:= bus of">>},[]},
-        {{1,216},{mid_expr,<<"true ->">>},[]},
-        {{1,229},{text,<<"Car">>},[]},
-        {{1,232},{mid_expr,<<"; false ->">>},[]},
-        {{1,248},{expr,<<"bus">>},[]},
-        {{1,259},{end_expr,<<"end">>},[]},
-        {{1,269},{text,<<"</p>">>},[]},
-        {{1,273},{end_expr,<<"end">>},[]},
-        {{1,283},{text,<<"</p>">>},[]},
-        {{1,287},{end_expr,<<"end">>},[]}
+    Expected = [
+        {text,<<"<h1>Title</h1>">>},
+        {start_expr,<<"case 1 of">>},
+        {mid_expr,<<"2 ->">>},
+        {text,<<"<p>Foo</p>">>},
+        {mid_expr,<<"; Bar ->">>},
+        {echo,<<"io:format(\"Print but not render me!~n\")">>},
+        {text,<<"<p>">>},
+        {start_expr,<<"case hello =:= world of">>},
+        {mid_expr,<<"true ->">>},
+        {expr,<<"hello">>},
+        {mid_expr,<<"; false ->">>},
+        {text,<<"<p>">>},
+        {start_expr,<<"case car =:= bus of">>},
+        {mid_expr,<<"true ->">>},
+        {text,<<"Car">>},
+        {mid_expr,<<"; false ->">>},
+        {expr,<<"bus">>},
+        {end_expr,<<"end">>},
+        {text,<<"</p>">>},
+        {end_expr,<<"end">>},
+        {text,<<"</p>">>},
+        {end_expr,<<"end">>}
     ],
     {ok, Tokens} = eel_tokenizer:tokenize(Bin, ?MODULE, []),
     ?assertEqual(Expected, Tokens).
