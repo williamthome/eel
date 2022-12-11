2ff8a13f910339b21b19f231fe21eb089ddfc3ac
williamthome <willegp88@gmail.com>
2022-12-11 17:40:58 -0300

    feat: multiline debug

--- src/eel_smart_engine.erl
+++ src/eel_smart_engine.erl
@@ -18,23 +18,36 @@
 %% Defines
 -define(token(Name), fun
     ({_, Bin}) -> {Name, Bin};
-    (Bin) -> {Name, Bin}
+    (Bin) ->      {Name, Bin}
 end).
--define(text, ?token(text)).
--define(expr, ?token(expr)).
--define(start, ?token(start_expr)).
--define(mid, ?token(mid_expr)).
--define('end', ?token(end_expr)).
--define(debug, ?token(debug)).
+-define(text,        ?token(text)).
+-define(expr,        ?token(expr)).
+-define(start_expr,  ?token(start_expr)).
+-define(mid_expr,    ?token(mid_expr)).
+-define(end_expr,    ?token(end_expr)).
+-define(debug,       ?token(debug)).
+-define(start_debug, ?token(start_debug)).
+-define(mid_debug,   ?token(mid_debug)).
+-define(end_debug,   ?token(end_debug)).
 
 %% Types
--type token() :: {text | expr | start_expr | mid_expr | end_expr | debug, binary()}.
--type static() :: list().
+-type token_name() :: text
+                      | expr
+                      | start_expr
+                      | mid_expr
+                      | end_expr
+                      | debug
+                      | start_debug
+                      | mid_debug
+                      | end_debug.
+-type token()   :: {token_name(), binary()}.
+-type static()  :: list().
 -type dynamic() :: list().
 
 %% State
 -record(state, {
-    tokens = [] :: [token()]
+    in     = text :: text | expr | {text | expr, debug},
+    tokens = []   :: [token()]
 }).
 
 %%%=============================================================================
@@ -44,24 +57,28 @@ end).
 init([]) ->
     {ok, #state{}}.
 
-handle_expr({_Pos, {<<"=">>, <<".">>}, {_ExpOut, <<>>}}, State) ->
-    {ok, State};
 handle_expr({_Pos, {<<"=">>, <<".">>}, Expr}, State) ->
     {ok, push(?expr(Expr), State)};
 handle_expr({_Pos, {<<"=">>, <<>>}, Expr}, State) ->
-    {ok, push(?start(Expr), State)};
-handle_expr({_Pos, {<<>>, <<>>}, Expr}, State) ->
-    {ok, push(?mid(Expr), State)};
-handle_expr({_Pos, {<<>>, <<".">>}, Expr}, State) ->
-    {ok, push(?'end'(Expr), State)};
-handle_expr({_Pos, {<<":">>, <<".">>}, Expr}, State) ->
+    {ok, push(?start_expr(Expr), State#state{in = expr})};
+handle_expr({_Pos, {<<>>, <<>>}, Expr}, #state{in = expr} = State) ->
+    {ok, push(?mid_expr(Expr), State)};
+handle_expr({_Pos, {<<>>, <<".">>}, Expr}, #state{in = expr} = State) ->
+    {ok, push(?end_expr(Expr), State)};
+handle_expr({_Pos, {<<":">>, <<":">>}, Expr}, State) ->
     {ok, push(?debug(Expr), State)};
+handle_expr({_Pos, {<<":">>, <<>>}, Expr}, #state{in = In} = State) ->
+    {ok, push(?start_debug(Expr), State#state{in = {In, debug}})};
+handle_expr({_Pos, {<<>>, <<>>}, Expr}, #state{in = {_, debug}} = State) ->
+    {ok, push(?mid_debug(Expr), State)};
+handle_expr({_Pos, {<<>>, <<":">>}, Expr}, #state{in = {In, debug}} = State) ->
+    {ok, push(?end_debug(Expr), State#state{in = In})};
 handle_expr({_Pos, {<<"%">>, <<>>}, _Expr}, State) ->
     {ok, State};
 handle_expr(Token, State) ->
     {error, {unknown_marker, {Token, State}}}.
 
-handle_text({_Pos, Text}, State) ->
+handle_text({_Pos, Text}, #state{in = In} = State) when In =:= text; In =:= expr ->
     {ok, push(?text(Text), State)}.
 
 handle_body(#state{tokens = Tokens}) ->
@@ -73,8 +90,8 @@ handle_body(#state{tokens = Tokens}) ->
 %%% Internal functions
 %%%=============================================================================
 
-push(Token, #state{tokens = Tokens}) ->
-    #state{tokens = [Token | Tokens]}.
+push(Token, #state{tokens = Tokens} = State) ->
+    State#state{tokens = [Token | Tokens]}.
 
 %% -----------------------------------------------------------------------------
 %% @private
@@ -87,8 +104,8 @@ parse_sd(Tokens) ->
     {[], SD} = parse_sd(Tokens, in_text, {[], []}),
     SD.
 
-parse_sd([{expr, _} = H | T], Where, {S, D}) ->
-    parse_sd(T, Where, {S, [H | D]});
+parse_sd([{expr, _} = H | T], In, {S, D}) ->
+    parse_sd(T, In, {S, [H | D]});
 parse_sd([{start_expr, _} = H | T], in_text, {S, D}) ->
     parse_sd(T, in_expr, {S, [H | D]});
 parse_sd([{start_expr, _} | _] = T, in_expr, {S, D}) ->
@@ -104,6 +121,12 @@ parse_sd([{text, _} = H | T], in_expr, {S, D}) ->
     parse_sd(T, in_expr, {S, [H | D]});
 parse_sd([{debug, _} = H | T], Where, {S, D}) ->
     parse_sd(T, Where, {S, [H | D]});
+parse_sd([{start_debug, _} = H | T], In, {S, D}) ->
+    parse_sd(T, {In, in_debug}, {S, [H | D]});
+parse_sd([{mid_debug, _} = H | T], {In, in_debug}, {S, D}) ->
+    parse_sd(T, {In, in_debug}, {S, [H | D]});
+parse_sd([{end_debug, _} = H | T], {In, in_debug}, {S, D}) ->
+    parse_sd(T, In, {S, [H | D]});
 parse_sd([], _, SD) ->
     {[], SD}.
 
@@ -143,13 +166,6 @@ parse_sd([], _, SD) ->
 handle_expr_test() ->
     [
         {
-            "Should return empty if no expression",
-            ?assertEqual(
-                {ok, #state{}},
-                handle_expr({{1, 1}, {<<"=">>, <<".">>}, {<<>>, <<>>}}, #state{})
-            )
-        },
-        {
             "Should return expr token",
             ?assertEqual(
                 {ok, #state{tokens = [{expr, <<"Foo">>}]}},
@@ -159,29 +175,50 @@ handle_expr_test() ->
         {
             "Should return start_expr token",
             ?assertEqual(
-                {ok, #state{tokens = [{start_expr, <<"Foo">>}]}},
+                {ok, #state{in = expr, tokens = [{start_expr, <<"Foo">>}]}},
                 handle_expr({{1, 1}, {<<"=">>, <<>>}, {<<"<%= Foo %>">>, <<"Foo">>}}, #state{})
             )
         },
         {
             "Should return mid_expr token",
             ?assertEqual(
-                {ok, #state{tokens = [{mid_expr, <<"Foo">>}]}},
-                handle_expr({{1, 1}, {<<>>, <<>>}, {<<"<% Foo %>">>, <<"Foo">>}}, #state{})
+                {ok, #state{in = expr, tokens = [{mid_expr, <<"Foo">>}]}},
+                handle_expr({{1, 1}, {<<>>, <<>>}, {<<"<% Foo %>">>, <<"Foo">>}}, #state{in = expr})
             )
         },
         {
             "Should return end_expr token",
             ?assertEqual(
-                {ok, #state{tokens = [{end_expr, <<"Foo">>}]}},
-                handle_expr({{1, 1}, {<<>>, <<".">>}, {<<"<% Foo .%>">>, <<"Foo">>}}, #state{})
+                {ok, #state{in = expr, tokens = [{end_expr, <<"Foo">>}]}},
+                handle_expr({{1, 1}, {<<>>, <<".">>}, {<<"<% Foo .%>">>, <<"Foo">>}}, #state{in = expr})
             )
         },
         {
             "Should return debug token",
             ?assertEqual(
                 {ok, #state{tokens = [{debug, <<"io:format(Foo)">>}]}},
-                handle_expr({{1, 1}, {<<":">>, <<".">>}, {<<"<%: io:format(Foo) .%>">>, <<"io:format(Foo)">>}}, #state{})
+                handle_expr({{1, 1}, {<<":">>, <<":">>}, {<<"<%: io:format(Foo) :%>">>, <<"io:format(Foo)">>}}, #state{})
+            )
+        },
+        {
+            "Should return start_debug token",
+            ?assertEqual(
+                {ok, #state{in = {text, debug}, tokens = [{start_debug, <<"Foo">>}]}},
+                handle_expr({{1, 1}, {<<":">>, <<>>}, {<<"<%= Foo %>">>, <<"Foo">>}}, #state{})
+            )
+        },
+        {
+            "Should return mid_debug token",
+            ?assertEqual(
+                {ok, #state{in = {text, debug}, tokens = [{mid_debug, <<"Foo">>}]}},
+                handle_expr({{1, 1}, {<<>>, <<>>}, {<<"<% Foo %>">>, <<"Foo">>}}, #state{in = {text, debug}})
+            )
+        },
+        {
+            "Should return end_debug token",
+            ?assertEqual(
+                {ok, #state{in = text, tokens = [{end_debug, <<"Foo">>}]}},
+                handle_expr({{1, 1}, {<<>>, <<":">>}, {<<"<% Foo :%>">>, <<"Foo">>}}, #state{in = {text, debug}})
             )
         },
         {
@@ -208,6 +245,13 @@ handle_text_test() ->
                 {ok, #state{tokens = [{text, <<"Foo">>}]}},
                 handle_text({{1, 1}, <<"Foo">>}, #state{})
             )
+        },
+        {
+            "Should throw error if text token when debugging",
+            ?assertError(
+                function_clause,
+                handle_text({{1, 1}, <<"Foo">>}, #state{in = {text, debug}})
+            )
         }
     ].
 
@@ -217,7 +261,7 @@ handle_body_test() ->
         "<%= case 1 of %>"
         "<% 2 -> %><p>Foo</p>"
         "<% ; Bar -> %>"
-            "<%: io:format(\"Print but not render me!~n\") .%>"
+            "<%: io:format(\"Print but not render me!~n\") :%>"
             "<p>"
                 "<%= case hello =:= world of %>"
                 "<% true -> %>"
@@ -226,6 +270,9 @@ handle_body_test() ->
                     "<p>"
                         "<%= case car =:= bus of %>"
                         "<% true -> %>"
+                            "<%: One = 1, %>"
+                            "<% Two = 1, %>"
+                            "<% io:format(\"Sum: ~n\", [One + Two]) :%>"
                             "Car"
                         "<% ; false -> %>"
                             "<%= bus .%>"
@@ -250,6 +297,9 @@ handle_body_test() ->
         {text,<<"<p>">>},
         {start_expr,<<"case car =:= bus of">>},
         {mid_expr,<<"true ->">>},
+        {start_debug,<<"One = 1,">>},
+        {mid_debug,<<"Two = 1,">>},
+        {end_debug,<<"io:format(\"Sum: ~n\", [One + Two])">>},
         {text,<<"Car">>},
         {mid_expr,<<"; false ->">>},
         {expr,<<"bus">>},
