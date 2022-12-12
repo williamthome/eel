c0d33a2e3ca0127b9272922a5f015315059faba8
williamthome <willegp88@gmail.com>
2022-12-11 22:05:34 -0300

    feat: raise unknown marker error

--- include/eel.hrl
+++ include/eel.hrl
@@ -1,1 +1,4 @@
--compile({parse_transform, eel_transform}).
+-define(unknown_marker_error(Token), begin
+    {{Ln, Col}, {_SMkr, _EMkr}, {Expr, _}} = Token,
+    error(unknown_marker, [{line, Ln}, {column, Col}, {expression, Expr}])
+end).
