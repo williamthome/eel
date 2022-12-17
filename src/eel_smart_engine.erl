31b1df81153284fd395514522b898fc12a9cdd27
williamthome <willegp88@gmail.com>
2022-12-16 21:28:26 -0300

    feat: parse tokens to statics and dynamics

--- src/eel_smart_engine.erl
+++ src/eel_smart_engine.erl
@@ -8,7 +8,8 @@
     markers/0,
     handle_expr/4,
     handle_text/3,
-    handle_body/1
+    handle_body/1,
+    compile/2
 ]).
 
 %% Includes
@@ -26,6 +27,12 @@
 -define(end_expr,    ?token(end_expr)).
 -define(comment,     ?token(comment)).
 -define(debug,       ?token(debug)).
+-define(is_expr(Token), is_tuple(Token) andalso
+                        (element(1, Token) =:= expr orelse
+                         element(1, Token) =:= start_expr orelse
+                         element(1, Token) =:= end_expr orelse
+                         element(1, Token) =:= nested_expr orelse
+                         element(1, Token) =:= debug)).
 
 %% Types
 -type token_name() :: text
@@ -76,9 +83,10 @@ handle_text(_Pos, Text, State) ->
     push(?text(Text), State).
 
 handle_body(#state{tokens = Tokens}) ->
-    _SD = parse_sd(lists:reverse(Tokens)),
-    % TODO: Continue parsing tokens to AST
     lists:reverse(Tokens).
+
+compile(Tokens, []) ->
+    parse_sd(Tokens).
 
 %%%=============================================================================
 %%% Internal functions
@@ -95,37 +103,83 @@ push(Token, #state{tokens = Tokens} = State) ->
 -spec parse_sd(Tokens :: [token()]) -> {static(), dynamic()}.
 
 parse_sd(Tokens) ->
-    parse_sd(Tokens, {[], []}).
+    {[], SD} = do_parse_sd(Tokens),
+    SD.
 
-parse_sd(Tokens, SD) ->
-    case do_parse_sd(Tokens, in_text, SD) of
+do_parse_sd(Tokens) ->
+    do_parse_sd(Tokens, in_text, undefined, {[], []}).
+
+do_parse_sd(Tokens, In, Prev, SD) ->
+    case do_parse_sd_1(Tokens, In, Prev, SD) of
         {[], {S, D}} ->
-            {lists:reverse(S), D};
-        {RestTokens, AccSD} ->
-            parse_sd(RestTokens, AccSD)
+            {[], {lists:reverse(S), lists:reverse(D)}};
+        {RestTokens, Prev1, AccSD} ->
+            do_parse_sd(RestTokens, in_text, Prev1, AccSD)
     end.
 
-do_parse_sd([{expr, _} = H | T], In, {S, D}) ->
-    do_parse_sd(T, In, {S, [H | D]});
-do_parse_sd([{start_expr, _} = H | T], in_text, {S, D}) ->
-    do_parse_sd(T, in_expr, {S, [H | D]});
-do_parse_sd([{start_expr, _} | _] = T, in_expr, {S, D}) ->
-    {Tokens, SD} = do_parse_sd(T, in_text, {[], []}),
-    do_parse_sd(Tokens, in_expr, {S, [{nested_expr, SD} | D]});
-do_parse_sd([{mid_expr, _} = H | T], in_expr, {S, D}) ->
-    do_parse_sd(T, in_expr, {S, [H | D]});
-do_parse_sd([{end_expr, _} = H | T], in_expr, {S, D}) ->
-    {T, {lists:reverse(S), lists:reverse([H | D])}};
-do_parse_sd([{text, _} = H | T], in_text, {S, D}) ->
-    do_parse_sd(T, in_text, {[H | S], D});
-do_parse_sd([{text, _} = H | T], in_expr, {S, D}) ->
-    do_parse_sd(T, in_expr, {S, [H | D]});
-do_parse_sd([{comment, _} | T], In, {S, D}) ->
-    do_parse_sd(T, In, {S, D});
-do_parse_sd([{debug, _} = H | T], In, {S, D}) ->
-    do_parse_sd(T, In, {S, [H | D]});
-do_parse_sd([], _, SD) ->
+% Single line expression
+do_parse_sd_1([{expr, _} = H | T], in_text, Prev, {S, D}) ->
+    do_parse_sd_2(T, H, in_text, Prev, {S, [H | D]});
+do_parse_sd_1([{expr, _} = H | T], in_expr, Prev, {S, [HD | D]}) ->
+    do_parse_sd_2(T, H, in_expr, Prev, {S, [[H | HD] | D]});
+% Multiline expression
+do_parse_sd_1([{start_expr, _} = H | T], in_text, Prev, {S, D}) ->
+    do_parse_sd_2(T, H, in_expr, Prev, {S, [[H] | D]});
+do_parse_sd_1([{start_expr, _} | _] = T, in_expr, Prev, SD) ->
+    parse_nested_sd(T, Prev, SD);
+do_parse_sd_1([{mid_expr, _} = H | T], in_expr, Prev, {S, [HD | D]}) ->
+    do_parse_sd_2(T, H, in_expr, Prev, {S, [[H | HD] | D]});
+do_parse_sd_1([{end_expr, _} = H | T], in_expr, _, {S, [HD | D]}) ->
+    {T, H, {S, [lists:reverse([H | HD]) | D]}};
+% Text
+do_parse_sd_1([{text, Text} = H | T], in_text, Prev, {S, D}) ->
+    do_parse_sd_2(T, H, in_text, Prev, {[Text | S], D});
+do_parse_sd_1([{text, _} | _] = T, in_expr, Prev, SD) ->
+    parse_nested_sd(T, Prev, SD);
+% Comment
+do_parse_sd_1([{comment, _} | T], In, Prev, {S, D}) ->
+    do_parse_sd_1(T, In, Prev, {S, D});
+% Debug
+do_parse_sd_1([{debug, _} = H | T], in_text, Prev, {S, D}) ->
+    do_parse_sd_2(T, H, in_text, Prev, {S, [H | D]});
+do_parse_sd_1([{debug, _} = H | T], in_expr, Prev, {S, [HD | D]}) ->
+    do_parse_sd_2(T, H, in_expr, Prev, {S, [[H | HD] | D]});
+% Done
+do_parse_sd_1([], _, _, SD) ->
     {[], SD}.
+
+do_parse_sd_2(Tokens, Curr, In, Prev, {S, D}) ->
+    SD = case should_push_empty_static(Prev, Curr, {S, D}) of
+             true -> {[<<>> | S], D};
+             false -> {S, D}
+         end,
+    do_parse_sd_1(Tokens, In, Curr, SD).
+
+should_push_empty_static(_, Curr, {[], _}) when ?is_expr(Curr) -> true;
+should_push_empty_static(Prev, Curr, {_, _}) -> ?is_expr(Prev) andalso
+                                                ?is_expr(Curr).
+
+% Nested
+parse_nested_sd(T, Prev, {S, [HD | D]}) ->
+    {Tokens, Nested} = parse_nested_sd_1(T, Prev, {[], []}),
+    H = {nested_expr, Nested},
+    do_parse_sd_1(Tokens, in_expr, H, {S, [[H | HD] | D]}).
+
+parse_nested_sd_1([{start_expr, _} | _] = T, Prev, {S, D}) ->
+    {Tokens, _, {S1, [H]}} = do_parse_sd_1(T, in_text, Prev, {S, []}),
+    parse_nested_sd_1(Tokens, H, {S1, [H | D]});
+parse_nested_sd_1([{mid_expr, _} | _] = T, _, {S, D}) ->
+    {T, {lists:reverse(S), lists:reverse(D)}};
+parse_nested_sd_1([{end_expr, _} | _] = T, _, {S, D}) ->
+    {T, {lists:reverse(S), lists:reverse(D)}};
+parse_nested_sd_1([{text, Text} = H | T], _, {S, D}) ->
+    parse_nested_sd_1(T, H, {[Text | S], D});
+parse_nested_sd_1([{expr, _} = H | T], _, {S, D}) ->
+    parse_nested_sd_1(T, H, {S, [H | D]});
+parse_nested_sd_1([{debug, _} = H | T], _, {S, D}) ->
+    parse_nested_sd_1(T, H, {S, [H | D]});
+parse_nested_sd_1([{comment, _} | T], Prev, {S, D}) ->
+    parse_nested_sd_1(T, Prev, {S, D}).
 
 % TODO: Retrieve funs vars
 % retrieve_vars(Bin) ->
@@ -226,6 +280,7 @@ handle_text_test() ->
 
 handle_body_test() ->
     Bin = <<
+        "<%= foo .%>"
         "<h1>Title</h1>"
         "<%: io:format(\"Print but not render me!~n\") :%>"
         "<%= case 1 of %>"
@@ -249,9 +304,11 @@ handle_body_test() ->
                 "<% end .%>"
             "</p>"
         "<% end .%>"
+        "<%= Foo = foo, Foo .%>"
         "<footer>Footer</footer>"
     >>,
     Expected = [
+        {expr, <<"foo">>},
         {text,<<"<h1>Title</h1>">>},
         {debug,<<"io:format(\"Print but not render me!~n\")">>},
         {start_expr,<<"case 1 of">>},
@@ -276,9 +333,69 @@ handle_body_test() ->
         {end_expr,<<"end">>},
         {text,<<"</p>">>},
         {end_expr,<<"end">>},
+        {expr,<<"Foo = foo, Foo">>},
         {text,<<"<footer>Footer</footer>">>}
     ],
-    Tokens = eel_tokenizer:tokenize(Bin, ?MODULE, []),
-    ?assertEqual(Expected, Tokens).
+    Result = eel_tokenizer:tokenize(Bin, ?MODULE, []),
+    ?assertEqual(Expected, Result).
+
+parse_sd_test() ->
+    Tokens = [
+        {expr, <<"foo">>},
+        {text,<<"<h1>Title</h1>">>},
+        {debug,<<"io:format(\"Print but not render me!~n\")">>},
+        {start_expr,<<"case 1 of">>},
+        {mid_expr,<<"2 ->">>},
+        {text,<<"<p>Foo</p>">>},
+        {mid_expr,<<"; Bar ->">>},
+        {text,<<"<p>">>},
+        {start_expr,<<"case hello =:= world of">>},
+        {mid_expr,<<"true ->">>},
+        {expr,<<"hello">>},
+        {mid_expr,<<"; false ->">>},
+        {text,<<"<p>">>},
+        {comment,<<"This is a comment">>},
+        {debug,<<"ignore_me">>},
+        {start_expr,<<"case car =:= bus of">>},
+        {mid_expr,<<"true ->">>},
+        {text,<<"Car">>},
+        {mid_expr,<<"; false ->">>},
+        {expr,<<"bus">>},
+        {end_expr,<<"end">>},
+        {text,<<"</p>">>},
+        {end_expr,<<"end">>},
+        {text,<<"</p>">>},
+        {end_expr,<<"end">>},
+        {expr,<<"Foo = foo, Foo">>},
+        {text,<<"<footer>Footer</footer>">>}
+    ],
+    Expected = {[<<>>,<<"<h1>Title</h1>">>,<<>>,<<>>,
+                   <<"<footer>Footer</footer>">>],
+                  [{expr,<<"foo">>},
+                   {debug,<<"io:format(\"Print but not render me!~n\")">>},
+                   [{start_expr,<<"case 1 of">>},
+                    {mid_expr,<<"2 ->">>},
+                    {nested_expr,{[<<"<p>Foo</p>">>],[]}},
+                    {mid_expr,<<"; Bar ->">>},
+                    {nested_expr,
+                        {[<<"<p>">>,<<"</p>">>],
+                         [[{start_expr,<<"case hello =:= world of">>},
+                           {mid_expr,<<"true ->">>},
+                           {expr,<<"hello">>},
+                           {mid_expr,<<"; false ->">>},
+                           {nested_expr,
+                               {[<<"<p>">>,<<>>,<<"</p>">>],
+                                [{debug,<<"ignore_me">>},
+                                 [{start_expr,<<"case car =:= bus of">>},
+                                  {mid_expr,<<"true ->">>},
+                                  {nested_expr,{[<<"Car">>],[]}},
+                                  {mid_expr,<<"; false ->">>},
+                                  {expr,<<"bus">>},
+                                  {end_expr,<<"end">>}]]}},
+                           {end_expr,<<"end">>}]]}},
+                    {end_expr,<<"end">>}],
+                   {expr,<<"Foo = foo, Foo">>}]},
+    Result = parse_sd(Tokens),
+    ?assertEqual(Expected, Result).
 
 -endif.
