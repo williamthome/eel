90b5fdce202a5b1ccff48e1e6ea0b8c8fdd92301
williamthome <willegp88@gmail.com>
2022-12-30 23:17:21 -0300

    refactor: review engine callbacks

--- src/eel_engine.erl
+++ src/eel_engine.erl
@@ -1,39 +1,37 @@
 -module(eel_engine).
 
+%%%=============================================================================
+%%% Types
+%%%=============================================================================
+
 -type line()        :: pos_integer().
 -type column()      :: pos_integer().
 -type expression()  :: binary().
 -type state()       :: term().
--type tokens()      :: term().
+-type token()       :: term().
 -type static()      :: [binary()].
+-type dynamic()     :: [token()].
 -type ast()         :: list().
 -type position()    :: {line(), column()}.
 -type marker()      :: {Start :: nonempty_string(), End :: nonempty_string()}.
 -type expressions() :: {Outside :: expression(), Inside :: expression()}.
 -type options()     :: map().
 
--export_type([
-    line/0,
-    column/0,
-    expression/0,
-    state/0,
-    tokens/0,
-    static/0,
-    ast/0,
-    position/0,
-    marker/0,
-    expressions/0,
-    options/0
-]).
+-export_type([line/0, column/0, expression/0, state/0, token/0, static/0,
+              dynamic/0, ast/0, position/0, marker/0, expressions/0, options/0]).
 
--callback init(options()) -> state().
+%%%=============================================================================
+%%% Callbacks
+%%%=============================================================================
 
 -callback markers() -> [marker()].
+-callback init(options()) -> state().
 
+%% tokenize
 -callback handle_expr(position(), marker(), expressions(), state()) -> state().
-
 -callback handle_text(position(), binary(), state()) -> state().
+-callback handle_body(state()) -> {static(), dynamic()}.
 
--callback handle_body(state()) -> ast().
-
--callback handle_compile(tokens(), options()) -> {static(), ast()}.
+%% compile
+-callback handle_compile(token(), state()) -> state().
+-callback handle_ast(state()) -> ast().
