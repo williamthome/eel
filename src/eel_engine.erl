fdc8112e86f65472b0ecb5c50a0e5773bb1a0c74
williamthome <willegp88@gmail.com>
2023-02-25 20:41:29 -0300

    refactor: rename types

--- src/eel_engine.erl
+++ src/eel_engine.erl
@@ -10,30 +10,30 @@
 %%% Types
 %%%=============================================================================
 
--type line()        :: pos_integer().
--type column()      :: pos_integer().
--type expression()  :: binary().
--type state()       :: term().
--type token()       :: term().
--type static()      :: [binary()].
--type dynamic()     :: [token()].
--type ast()         :: list().
--type position()    :: {line(), column()}.
--type marker_id()   :: atom().
--type marker()      :: nonempty_string().
--type marker_def()  :: {marker_id(), {Start :: marker(), End :: marker()}}.
--type expressions() :: {Outer :: expression(), Inner :: expression()}.
--type options()     :: map().
+-type line()          :: pos_integer().
+-type column()        :: pos_integer().
+-type expression()    :: binary().
+-type state()         :: term().
+-type token()         :: term().
+-type static()        :: [binary()].
+-type dynamic()       :: [token()].
+-type ast()           :: list().
+-type position()      :: {line(), column()}.
+-type marker_id()     :: atom().
+-type marker_symbol() :: nonempty_string().
+-type marker()        :: {marker_id(), {Start :: marker_symbol(), End :: marker_symbol()}}.
+-type expressions()   :: {Outer :: expression(), Inner :: expression()}.
+-type options()       :: map().
 
 -export_type([line/0, column/0, expression/0, state/0, token/0, static/0,
-              dynamic/0, ast/0, position/0, marker_id/0, marker/0, marker_def/0,
+              dynamic/0, ast/0, position/0, marker_id/0, marker_symbol/0, marker/0,
               expressions/0, options/0]).
 
 %%%=============================================================================
 %%% Callbacks
 %%%=============================================================================
 
--callback markers() -> [marker_def()].
+-callback markers() -> [marker()].
 -callback init(options()) -> state().
 
 %% tokenize
