e667a25380c69cff09611db0ea370af6368cacf5
williamthome <willegp88@gmail.com>
2023-03-04 04:27:03 -0300

    refactor: format code

--- include/eel_engine.hrl
+++ include/eel_engine.hrl
@@ -1,7 +1,6 @@
--define(unknown_marker_error(Token), begin
-    {{Ln, Col}, Marker, Expr} = Token,
-    error(unknown_marker, [{line, Ln},
-                           {column, Col},
-                           {expression, Expr},
-                           {marker, Marker}])
-end).
+-define(unknown_marker_error(Token),
+    begin {{Ln, Col}, Marker, Expr} = Token,
+          error(unknown_marker, [{line, Ln},
+                                 {column, Col},
+                                 {expression, Expr},
+                                 {marker, Marker}]) end).
