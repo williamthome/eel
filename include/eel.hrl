f834d5000e1e772d4bda56667e6bdd9e737f05d8
williamthome <willegp88@gmail.com>
2023-03-19 02:44:54 -0300

    feat: fun from file or binary

--- include/eel.hrl
+++ include/eel.hrl
@@ -1,4 +1,29 @@
--define(DEFAULT_ENGINE, eel_smart_engine).
--define(DEFAULT_ENGINE_OPTS, #{
-    % TODO
-}).
+-compile({parse_transform, eel_transform}).
+
+% Binary
+
+-define(eel_fun_from_bin(FunName, Bin, Opts, FunOpts),
+    -eel_fun({FunName, {bin, Bin, Opts}, FunOpts})
+).
+
+-define(eel_fun_from_bin(FunName, Bin, Opts),
+    ?eel_fun_from_bin(FunName, Bin, Opts, #{eval => true})
+).
+
+-define(eel_fun_from_bin(FunName, Bin),
+    ?eel_fun_from_bin(FunName, Bin, #{})
+).
+
+% File
+
+-define(eel_fun_from_file(FunName, Filename, Opts, FunOpts),
+    -eel_fun({FunName, {file, Filename, Opts}, FunOpts})
+).
+
+-define(eel_fun_from_file(FunName, Filename, Opts),
+    ?eel_fun_from_file(FunName, Filename, Opts, #{eval => true})
+).
+
+-define(eel_fun_from_file(FunName, Filename),
+    ?eel_fun_from_file(FunName, Filename, #{})
+).
