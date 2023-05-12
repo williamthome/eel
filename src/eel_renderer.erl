2b019a09402bad82fff56a7d51eace50e2b02ae1
williamthome <willegp88@gmail.com>
2023-05-11 22:35:41 -0300

    feat: snapshot return

--- src/eel_renderer.erl
+++ src/eel_renderer.erl
@@ -7,10 +7,10 @@
 -module(eel_renderer).
 
 %% API functions
--export([render/1, render/2, render/3]).
+-export([render/1, render/2, render/3, snapshot/3, snapshot/4, snapshot/5]).
 
 %% Types
--export_type([bindings/0, snapshot/0, tokens/0, result/0]).
+-export_type([bindings/0, snapshot/0, result/0]).
 
 %% Includes
 -include("eel_core.hrl").
@@ -21,17 +21,10 @@
 %% Type
 -type bindings() :: map().
 -type snapshot() :: #{static   => eel_engine:static(),
-                      ast      => eel_engine:ast(),
-                      vars     => map(),
                       dynamic  => [binary()],
-                      bindings => bindings()}.
--type tokens()   :: {eel_engine:static(), eel_engine:ast()}
-                  | snapshot()
-                  | #{static => eel_engine:static(), 
-                      ast    => eel_engine:ast()}
-                  | #{static => eel_engine:static(),
-                      ast    => eel_engine:ast(),
-                      vars   => term()}.
+                      ast      => eel_engine:ast(),
+                      bindings => bindings(),
+                      vars     => [atom()]}.
 -type options()  :: map().
 -type result()   :: {ok, snapshot()}.
 
@@ -43,46 +36,40 @@
 %% @doc render/1.
 %% @end
 %% -----------------------------------------------------------------------------
--spec render(Payload) -> Result when
-    Payload :: tokens(),
-    Result  :: result().
+-spec render(Snapshot) -> Result when
+    Snapshot :: snapshot(),
+    Result   :: result().
 
-render(Payload) ->
-    render(Payload, ?DEFAULT_ENGINE_OPTS).
+render(Snapshot) ->
+    render(#{}, Snapshot, ?DEFAULT_ENGINE_OPTS).
 
 %% -----------------------------------------------------------------------------
 %% @doc render/2.
 %% @end
 %% -----------------------------------------------------------------------------
--spec render(Arg1, Arg2) -> Result when
-    Arg1   :: tokens() | bindings(),
-    Arg2   :: tokens() | options(),
-    Result :: result().
+-spec render(Bindings, Snapshot) -> Result when
+    Bindings :: bindings(),
+    Snapshot :: snapshot(),
+    Result   :: result().
 
-render(#{static := _, ast := _} = Tokens, Opts) ->
-    render(#{}, Tokens, Opts);
-render(Bindings, #{static := _, ast := _} = Tokens) ->
-    render(Bindings, Tokens, ?DEFAULT_ENGINE_OPTS);
-render(Tokens, Opts) when is_map(Opts) ->
-    render(#{}, Tokens, Opts);
-render(Bindings, {Static, AST}) ->
-    render(Bindings, {Static, AST}, ?DEFAULT_ENGINE_OPTS).
+render(Bindings, Snapshot) ->
+    render(Bindings, Snapshot, ?DEFAULT_ENGINE_OPTS).
 
 %% -----------------------------------------------------------------------------
 %% @doc render/3.
 %% @end
 %% -----------------------------------------------------------------------------
--spec render(Bindings, Tokens, Opts) -> Result when
+-spec render(Bindings, Snapshot, Opts) -> Result when
     Bindings :: bindings(),
-    Tokens   :: tokens(),
+    Snapshot :: snapshot(),
     Opts     :: options(),
     Result   :: result().
 
 render(Changes0, #{static   := Static,
-                   ast      := AST,
-                   vars     := Vars,
                    dynamic  := DynamicSnap,
-                   bindings := BindingsSnap}, Opts) ->
+                   ast      := AST,
+                   bindings := BindingsSnap,
+                   vars     := Vars}, Opts) ->
     Changes = normalize_bindings(Changes0, Opts),
     ChangesKeys = maps:keys(Changes),
     Bindings = maps:merge(BindingsSnap, Changes),
@@ -99,22 +86,54 @@ render(Changes0, #{static   := Static,
             end,
             Vars
         ),
-    render_result(Static, AST, Vars, Dynamic, Bindings);
-render(Bindings0, #{static := Static,
-                    ast    := AST,
-                    vars   := Vars}, Opts) ->
-    Bindings1 = normalize_bindings(Bindings0, Opts),
-    Bindings = erl_eval:add_binding('Bindings', Bindings1, Bindings1),
-    Dynamic = lists:map(fun(Exprs) -> eval(Exprs, Bindings) end, AST),
-    render_result(Static, AST, Vars, Dynamic, Bindings);
-render(Bindings, #{static := _, ast := AST} = Snapshot, Opts) ->
+    {ok, snapshot(Static, Dynamic, AST, Bindings, Vars)}.
+
+%% -----------------------------------------------------------------------------
+%% @doc snapshot/3.
+%% @end
+%% -----------------------------------------------------------------------------
+-spec snapshot(Static, Dynamic, AST) -> Result when
+    Static   :: eel_engine:static(),
+    Dynamic  :: [binary()],
+    AST      :: eel_engine:ast(),
+    Result   :: snapshot().
+
+snapshot(Static, Dynamic, AST) ->
+    snapshot(Static, Dynamic, AST, #{}).
+
+%% -----------------------------------------------------------------------------
+%% @doc snapshot/4.
+%% @end
+%% -----------------------------------------------------------------------------
+-spec snapshot(Static, Dynamic, AST, Bindings) -> Result when
+    Static   :: eel_engine:static(),
+    Dynamic  :: [binary()],
+    AST      :: eel_engine:ast(),
+    Bindings :: bindings(),
+    Result   :: snapshot().
+
+snapshot(Static, Dynamic, AST, Bindings) ->
     Vars = eel_compiler:ast_vars(AST),
-    render(Bindings, Snapshot#{vars => Vars}, Opts);
-render(Bindings, {Static, AST}, Opts) ->
-    Snapshot = #{static => Static,
-                 ast    => AST,
-                 vars   =>  eel_compiler:ast_vars(AST)},
-    render(Bindings, Snapshot, Opts).
+    snapshot(Static, Dynamic, AST, Bindings, Vars).
+
+%% -----------------------------------------------------------------------------
+%% @doc snapshot/5.
+%% @end
+%% -----------------------------------------------------------------------------
+-spec snapshot(Static, Dynamic, AST, Bindings, Vars) -> Result when
+    Static   :: eel_engine:static(),
+    Dynamic  :: [binary()],
+    AST      :: eel_engine:ast(),
+    Bindings :: bindings(),
+    Vars     :: [atom()],
+    Result   :: snapshot().
+
+snapshot(Static, Dynamic, AST, Bindings, Vars) ->
+    #{static => Static,
+      dynamic => Dynamic,
+      ast => AST,
+      bindings => Bindings,
+      vars => Vars}.
 
 %%%=============================================================================
 %%% Internal functions
@@ -167,7 +186,7 @@ capitalize_1(Atom, Key, Opts) when is_atom(Atom) ->
     capitalize_1(erlang:atom_to_binary(Atom), Key, Opts);
 capitalize_1(List, Key, Opts) when is_list(List) ->
     capitalize_1(erlang:list_to_binary(List), Key, Opts).
- 
+
 capitalize_2(<<$_, H, T/binary>>, Opts, Acc) when H >= $a, H =< $z ->
     capitalize_2(T, Opts, <<Acc/binary, (H - 32)>>);
 capitalize_2(<<H, T/binary>>, Opts, Acc) ->
@@ -179,16 +198,6 @@ to_atom(Bin, #{safe_atoms := true}) ->
     erlang:binary_to_existing_atom(Bin);
 to_atom(Bin, #{}) ->
     erlang:binary_to_atom(Bin).
-
-render_result(Static, AST, Vars, Dynamic, Bindings) ->
-    Snapshot = #{
-        static => Static,
-        ast => AST,
-        vars => Vars,
-        dynamic => Dynamic,
-        bindings => Bindings
-    },
-    {ok, Snapshot}.
 
 %%%=============================================================================
 %%% Tests
