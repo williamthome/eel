092d7649db1db20204cbbcb54c29ac7c87ff34c3
williamthome <willegp88@gmail.com>
2023-05-25 00:44:11 -0300

    feat: change parse transformation strategy

--- src/eel_transform.erl
+++ src/eel_transform.erl
@@ -3,99 +3,33 @@
 -export([parse_transform/2]).
 
 parse_transform(Forms, _Options) ->
-    lists:map(
-        fun(Form) ->
-            case erl_syntax:type(Form) =:= function andalso
-                 erl_syntax:function_clauses(Form)
-            of
-                [{clause, Pos, Patterns, Guards, Body}] ->
-                    LastBodyElem = lists:last(Body),
-                    case erl_syntax:type(LastBodyElem) =:= tuple andalso
-                         erl_syntax:tuple_elements(LastBodyElem)
-                    of
-                        [{atom, _, eel}, Defs] ->
-                            [{atom, _, Action}, ActionArgs] = erl_syntax:tuple_elements(Defs),
-                            EElAction = eel(Action, erl_syntax:tuple_elements(ActionArgs)),
-                            NewBody = lists:droplast(Body) ++ [erl_syntax:revert(EElAction)],
-                            NewForm = setelement(tuple_size(Form), Form, [
-                                {clause, Pos, Patterns, Guards, NewBody}
-                            ]),
-                            NewForm;
-                        _ ->
-                            Form
-                    end;
-                _ ->
-                    Form
-            end
+    deepmap(
+        fun
+            ({call, _, {remote, _, {atom, _, eel}, {atom, _, compile}}, _} = Form0) ->
+                {value, Form, []} = erl_eval:expr(Form0, []),
+                erl_syntax:revert(erl_syntax:abstract(Form));
+            (Form) ->
+                Form
         end,
         Forms
     ).
 
-eel(fun_from_binary, [{var, _, 'Bindings'}, Text, Opts]) ->
-    eel_fun_from({binary, {parserl_trans:eval(Text), Opts}});
-eel(fun_from_file, [{var, _, 'Bindings'}, Filename, Opts]) ->
-    eel_fun_from({file, {parserl_trans:eval(Filename), Opts}}).
+deepmap(Fun, Forms) when is_function(Fun, 1), is_list(Forms) ->
+    do_deepmap(Forms, Fun).
 
-eel_fun_from({binary, {Text0, Opts0}}) ->
-    Bin = flatten_ws(4, Text0),
-    Opts = parserl_trans:eval(Opts0),
-    Result = maps:get(result, Opts),
-    do_eel_fun(Result, {binary, Bin, Opts});
-eel_fun_from({file, {Filename, Opts0}}) ->
-    Opts = parserl_trans:eval(Opts0),
-    Result = maps:get(result, Opts),
-    do_eel_fun(Result, {file, Filename, Opts}).
-
-do_eel_fun(render, Defs) ->
-    {ok, Snapshot} = compile(Defs),
-    parserl_trans:quote(
-        "eel_renderer:render(Bindings, _@snapshot)"
-        , #{ snapshot => Snapshot } );
-do_eel_fun(eval, Defs) ->
-    {ok, Snapshot} = compile(Defs),
-    parserl_trans:quote(
-        "eel_evaluator:eval(eel_renderer:render(Bindings, _@snapshot))"
-        , #{ snapshot => Snapshot } ).
-
-flatten_ws(N, Text) ->
-    RE = lists:flatten(["\n", lists:duplicate(N, " ")]),
-    re:replace(Text, RE, <<>>, [global, {return, binary}, unicode]).
-
-compile({binary, Bin, Opts}) ->
-    compile(Bin, Opts);
-compile({file, {priv, Filename}, Opts}) ->
-    case application:get_application() of
-        {ok, App} ->
-            compile_priv_file(App, Filename, Opts);
-        undefined ->
-            error(undefined_application)
-    end;
-compile({file, {priv, App, Filename}, Opts})  ->
-    compile_priv_file(App, Filename, Opts);
-compile({file, Filename, Opts}) ->
-    compile_file(Filename, Opts).
-
-compile_priv_file(App, Filename0, Opts) when is_atom(App),
-                                             ( is_binary(Filename0) orelse
-                                               is_list(Filename0) ) ->
-    Filename = filename:join([code:priv_dir(App), Filename0]),
-    compile_file(Filename, Opts).
-
-compile(Bin, Opts) ->
-    case eel:compile(Bin, Opts) of
-        {ok, Result} ->
-            {ok, Result};
-        {error, Reason} ->
-            error(Reason, #{ bin => Bin
-                           , opts => Opts })
-    end.
-
-compile_file(Filename, Opts) ->
-    case eel:compile_file(Filename, Opts) of
-        {ok, Result} ->
-            {ok, Result};
-        {error, Reason} ->
-            error({Reason, #{ filename => Filename
-                            , opts => Opts }})
-    end.
-
+do_deepmap({match, Pos, A, B}, F) ->
+    F({match, Pos, do_deepmap(A, F), do_deepmap(B, F)});
+do_deepmap({call, Pos, Fun, Args}, F) ->
+    F({call, Pos, Fun, do_deepmap(Args, F)});
+do_deepmap({clause, CPos, CPattern, CGuards, CBody}, F) ->
+    F({clause, CPos, do_deepmap(CPattern, F), do_deepmap(CGuards, F), do_deepmap(CBody, F)});
+do_deepmap({function, Pos, Name, Arity, Clauses}, F) ->
+    F({function, Pos, Name, Arity, do_deepmap(Clauses, F)});
+do_deepmap({'case', Pos, Cond, Clauses}, F) ->
+    F({'case', Pos, do_deepmap(Cond, F), do_deepmap(Clauses, F)});
+do_deepmap({'if', Pos, Clauses}, F) ->
+    F({'if', Pos, do_deepmap(Clauses, F)});
+do_deepmap(Forms, F) when is_list(Forms) ->
+    lists:map(fun(Form) -> do_deepmap(Form, F) end, Forms);
+do_deepmap(Form, F) ->
+    F(Form).
